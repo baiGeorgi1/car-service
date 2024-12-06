9255f93701c50c07fcff9960721c01f2ae629b79
Georgi <gogolino1982@gmail.com>
2024-12-06 11:40:22 +0200

    Catalog path fixes

--- car-service/src/app/pages/pages-routing.module.ts
+++ car-service/src/app/pages/pages-routing.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ServiceComponent } from './service/service.component';
-import { HomeComponent } from './home/home.component';
-import { NewestCarsComponent } from './newest-cars/newest-cars.component';
-import { FeaturedCarsComponent } from './featured-cars/featured-cars.component';
-import { ContactsComponent } from './contacts/contacts.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ServiceComponent } from "./service/service.component";
+import { HomeComponent } from "./home/home.component";
+import { NewestCarsComponent } from "./newest-cars/newest-cars.component";
+import { CatalogComponent } from "./catalog/catalog.component";
+import { ContactsComponent } from "./contacts/contacts.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'service', component: ServiceComponent },
-  { path: 'new-cars', component: NewestCarsComponent },
-  { path: 'featured-cars', component: FeaturedCarsComponent },
-  { path: 'contacts', component: ContactsComponent },
-  // { path: '404', component: ErrorComponent },
-  // { path: '**', redirectTo: '/404' },
+    { path: "", component: HomeComponent },
+    { path: "service", component: ServiceComponent },
+    { path: "new-cars", component: NewestCarsComponent },
+    { path: "catalog", component: CatalogComponent },
+    { path: "contacts", component: ContactsComponent },
+    // { path: '404', component: ErrorComponent },
+    // { path: '**', redirectTo: '/404' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class PagesRoutingModule {}
--- car-service/src/app/pages/pages.module.ts
+++ car-service/src/app/pages/pages.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeComponent } from './home/home.component';
-import { ServiceComponent } from './service/service.component';
-import { NewestCarsComponent } from './newest-cars/newest-cars.component';
-import { FeaturedCarsComponent } from './featured-cars/featured-cars.component';
-import { PagesRoutingModule } from './pages-routing.module';
-import { CoreModule } from '../core/core.module';
-import { ContactsComponent } from './contacts/contacts.component';
-import { ErrorComponent } from '../error/error.component';
+import { HomeComponent } from "./home/home.component";
+import { ServiceComponent } from "./service/service.component";
+import { NewestCarsComponent } from "./newest-cars/newest-cars.component";
+import { CatalogComponent } from "./catalog/catalog.component";
+import { PagesRoutingModule } from "./pages-routing.module";
+import { CoreModule } from "../core/core.module";
+import { ContactsComponent } from "./contacts/contacts.component";
+import { ErrorComponent } from "../error/error.component";
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    ServiceComponent,
-    NewestCarsComponent,
-    FeaturedCarsComponent,
-    ContactsComponent,
-    ErrorComponent,
-  ],
-  imports: [CommonModule, CoreModule, PagesRoutingModule],
+    declarations: [
+        HomeComponent,
+        ServiceComponent,
+        NewestCarsComponent,
+        CatalogComponent,
+        ContactsComponent,
+        ErrorComponent,
+    ],
+    imports: [CommonModule, CoreModule, PagesRoutingModule],
 })
 export class PagesModule {}
