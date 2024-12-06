0832d102ea9de0393f268e0851ead3ad9b8171c4
Georgi <gogolino1982@gmail.com>
2024-12-06 18:34:35 +0200

    login form created

--- car-service/src/app/app-routing.module.ts
+++ car-service/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './error/error.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ErrorComponent } from "./error/error.component";
 
 const routes: Routes = [
-  // { path: "", pathMatch: "full", redirectTo: "/" }
-  { path: '404', component: ErrorComponent },
+    // { path: "/", pathMatch: "full", redirectTo: "/" },
+    { path: "404", component: ErrorComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
